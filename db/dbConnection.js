d3b4373aca3a8edfb0c1a0539634030a98a881f0
YousefEbad <yousefomar6464@gmail.com>
2025-10-08 00:46:09 +0300

    atlassss

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-export const dbConnection = mongoose.connect(process.env.MONGO_URI)
+export const dbConnection = mongoose.connect("mongodb+srv://concept12:ABC6453abc@cluster0.g0ytlmy.mongodb.net/")
 .then(() => console.log("DB Connected")).catch((err) => console.log(err));
 
 
